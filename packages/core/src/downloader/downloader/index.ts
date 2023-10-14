2e5c85e383e0fcdd01594195f1d0e9d43a15a09d
張智翔 <ch.chang.job@gmail.com>
2023-10-14 18:21:09 +0800

    feat(core): add outdir options and call download func

---
+++ packages/core/src/downloader/downloader/index.ts
@@ -0,0 +1,8 @@
+import { type Status, type Package } from '../../constants'
+
+export const download = async (
+  packages: Package[],
+  registry: string,
+  outDir: string,
+  callback?: (status: Status, extra: unknown) => void
+): Promise<void> => {}
--- packages/core/src/downloader/index.ts
+++ packages/core/src/downloader/index.ts
@@ -1,21 +1,33 @@
 /* TODO: 解除因未開發而抑制的 eslint 警告 */
 /* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { type DenMgr, type Status } from '../constants'
-import { DEFAULT_REGISTRY } from '../defaults'
+import { Status, type DenMgr } from '../constants'
+import { DEFAULT_OUT_DIR, DEFAULT_REGISTRY } from '../defaults'
 import { detectDenMgr, detectLockFile } from './detector'
 import { parsePackages } from './parser'
+import { download as innerDownload } from './downloader'
 
 interface Options {
   denMgr?: DenMgr
   registry?: string
-  callback?: (status: Status) => void
+  outDir?: string
+  callback?: (status: Status, extra: unknown) => void
 }
 
 export const download = async (options: Options): Promise<void> => {
+  const callback = options.callback
+
   const registry = options.registry ?? DEFAULT_REGISTRY
+  const outDir = options.outDir ?? DEFAULT_OUT_DIR
+
   const denMgr = options.denMgr ?? detectDenMgr()
+  callback?.(Status.DependencyManagerDetected, denMgr)
 
   const lockFile = detectLockFile(denMgr)
+  callback?.(Status.LockFileDetected, lockFile)
+
   const packages = parsePackages(lockFile)
+  callback?.(Status.LockFileParsed, packages)
+
+  await innerDownload(packages, registry, outDir)
 }
