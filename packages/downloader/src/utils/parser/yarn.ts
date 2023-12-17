b9c8f1a2be6d70d4725fb20851d49f6219fc20a4
CH-Chang <ch.chang.software@gmail.com>
2023-12-17 20:39:22 +0800

    feat(downloader): use official parser for yarn

--- packages/downloader/src/utils/parser/yarn.ts
+++ packages/downloader/src/utils/parser/yarn.ts
@@ -2,19 +2,14 @@ import { type Package } from '@dep-mgr/share'
 import { type ParsePackagesFunction } from './share'
 import { LockFile } from '../../constants'
 import { ParserError, ParserErrorCode } from '../../error/parser-error'
-import { extractPkgsFromYarnLockV1 } from 'snyk-nodejs-lockfile-parser'
 import { readFileSync } from 'fs'
 
 const parseYarnV1Packages = (lockFileContent: string): Package[] => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const parsed = extractPkgsFromYarnLockV1(lockFileContent)
-  return []
+  throw new ParserError(ParserErrorCode.NOT_IMPLEMENT)
 }
 
 const parseYarnV2Packages = (lockFileContent: string): Package[] => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const parsed = extractPkgsFromYarnLockV1(lockFileContent)
-  return []
+  throw new ParserError(ParserErrorCode.NOT_IMPLEMENT)
 }
 
 export const parsePackages: ParsePackagesFunction = async (
