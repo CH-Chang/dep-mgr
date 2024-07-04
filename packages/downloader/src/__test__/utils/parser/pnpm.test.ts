64d1b4191e3b5f9541460f24d127b8dbcbc609dd
CH-Chang <ch.chang.software@gmail.com>
2024-07-04 22:16:03 +0800

    test(downloader): add pnpm parser test case

--- packages/downloader/src/__test__/utils/parser/pnpm.test.ts
+++ packages/downloader/src/__test__/utils/parser/pnpm.test.ts
@@ -1,7 +1,17 @@
-// eslint-disable-next-line lodash/prefer-noop
+import { size } from 'lodash'
+import { LockFile } from '../../../constants'
+import { parsePackages } from '../../../utils/parser/pnpm'
+import path from 'path'
+
 describe('測試 pnpm lockfile 解析器', (): void => {
-  it('佔位測試', (): void => {
+  it('佔位測試', async (): Promise<void> => {
     expect.assertions(1)
-    expect(true).toBe(true)
+
+    const lockFile = LockFile.PnpmYamlLockFile
+    const lockFilePath = path.resolve(__dirname, 'cases', 'pnpm', 'pnpm-lock.yaml')
+
+    const packages = await parsePackages(lockFile, lockFilePath)
+
+    expect(size(packages)).toBeGreaterThan(0)
   })
 })
