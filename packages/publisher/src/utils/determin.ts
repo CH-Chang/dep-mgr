d3547a350ece5f6b6da62a0bf5a88ddfe9e663a2
CH-Chang <ch.chang.software@gmail.com>
2024-07-05 21:47:37 +0800

    refactor(publisher): move determin relative function to independent ts file

---
+++ packages/publisher/src/utils/determin.ts
@@ -0,0 +1,58 @@
+import { type LocalPackage } from '../constants'
+import { isUndefined, isNull } from 'lodash'
+import { PublishError, PublishErrorCode } from '../errors/publish-error'
+import { extractPackageJsonFromTarball } from './extractor'
+import { retryAsync } from 'ts-retry'
+import fetch from 'node-fetch'
+import urlJoin from 'url-join'
+import commonJson from 'comment-json'
+
+interface PartialPackageJson {
+  publishConfig?: {
+    registry?: string
+    tag?: string
+    access?: string
+  }
+}
+
+export const isRegistryExistsPackage = async (
+  localPackage: LocalPackage,
+  registry: string
+): Promise<boolean> => {
+  const { organization, name, version } = localPackage
+
+  const url = isUndefined(organization)
+    ? urlJoin(registry, name, '-', `${name}-${version}.tgz`)
+    : urlJoin(registry, organization, name, '-', `${name}-${version}.tgz`)
+
+  const response = await retryAsync(
+    async () => await fetch(url),
+    { delay: 100, maxTry: 3 }
+  )
+
+  return response.status === 200
+}
+
+export const isPackageExistsPublishConfig = async (
+  localPackage: LocalPackage,
+  registry: string
+): Promise<boolean> => {
+  const { location } = localPackage
+
+  const packageJsonContent = await extractPackageJsonFromTarball(location)
+  if (isNull(packageJsonContent)) {
+    throw new PublishError(PublishErrorCode.INVALID_PACKAGE_TARBALL)
+  }
+
+  const packageJson = commonJson.parse(packageJsonContent) as PartialPackageJson
+
+  const { publishConfig } = packageJson
+  if (!isUndefined(publishConfig)) {
+    const { registry: packagePublishConfigRegistry } = publishConfig
+    if (registry !== packagePublishConfigRegistry) {
+      return true
+    }
+  }
+
+  return false
+}
