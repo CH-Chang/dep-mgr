1afb48b0ba3a2d703aa99f507d6cf4223f6062ca
CH-Chang <ch.chang.software@gmail.com>
2024-07-23 22:39:03 +0800

    feat(downloader): support multiple lockfile version for same package manager

--- packages/downloader/src/utils/parser/pnpm.ts
+++ packages/downloader/src/utils/parser/pnpm.ts
@@ -2,9 +2,10 @@ import { type Package } from '@dep-mgr/share'
 import { type LockFile } from '../../constants'
 import { type ParsePackagesFunction } from './share'
 import { ParserError, ParserErrorCode } from '../../error/parser-error'
-import { isNull, isUndefined, chain, includes, split } from 'lodash'
+import { isNull, isUndefined, chain, includes, split, parseInt } from 'lodash'
 import { readWantedLockfile } from '@pnpm/lockfile-file'
-import { parse } from '@pnpm/dependency-path'
+import { parse as dependencyPathParseV2 } from '@pnpm/dependency-path2'
+import { parse as dependencyPathParseV5 } from '@pnpm/dependency-path5'
 import path from 'path'
 
 export const parsePackages: ParsePackagesFunction = async (
@@ -19,10 +20,17 @@ export const parsePackages: ParsePackagesFunction = async (
     throw new ParserError(ParserErrorCode.PARSE_PNPM_LOCK_FILE_ERROR)
   }
 
-  const { packages } = parsed
+  const { packages, lockfileVersion } = parsed
   if (isUndefined(packages)) {
     return []
   }
+
+  // @pnpm/dependency-path5 after v2.1.7 drop support for pnpm lockfile version 5
+  // https://github.com/pnpm/pnpm/blob/main/packages/dependency-path/CHANGELOG.md#217
+  const parse =
+    parseInt(lockfileVersion) >= 6
+      ? dependencyPathParseV5
+      : dependencyPathParseV2
 
   return chain(packages)
     .keys()
